1ade82c39181744dbf1a09990865d265339cdb8b
James Volmert <james.volmert@gmail.com>
2016-01-14 15:37:38 -0700

    assets

--- js/logic.js
+++ js/logic.js
@@ -1,3 +1,8 @@
 /**
- * Created by jamesvolmert on 1/14/16.
- */
+Hints for the first problem:
+    1. Define a function that:
+- is assigned to a variable named `sum`
+- has one parameter named `options`
+- returns the calculated sum of all of the values of all of the options passed in
+NOTE: use a loop to make this happen.
+*/
